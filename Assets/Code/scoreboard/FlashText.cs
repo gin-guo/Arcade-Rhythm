d7dd6728adb609782e065ec1d0d69666ea3f3276
anniethemoody <annieli0428@gmail.com>
2024-02-29 20:21:46 -0500

    update scoreboard (with bugs), currently trying to select all characters in the inputfield

--- Assets/Code/scoreboard/FlashText.cs
+++ Assets/Code/scoreboard/FlashText.cs
@@ -6,7 +6,7 @@ using TMPro;
 public class FlashText : MonoBehaviour
 {
     public TextMeshProUGUI displayText;
-    private string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private string alphabet = "abcdefghijklmnopqrstuvwxyz0123456789_";
     private int currentIndex = 0;
 
     private void Start()
@@ -16,11 +16,11 @@ public class FlashText : MonoBehaviour
 
     private void Update()
     {
-        /*if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             ConfirmSelection();
-        }*/
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             CycleNextCharacter();
         }
@@ -29,20 +29,29 @@ public class FlashText : MonoBehaviour
     //loop through each character in the alphabet
     private void CycleNextCharacter()
     {
+        //replace _ with 'a'
+        char[] charArray = displayText.text.ToCharArray();
+        //charArray[currentIndex];
+        if (charArray[currentIndex] == '_')
+        {
+            charArray[currentIndex] = alphabet[0];
+            displayText.text = new string(charArray);
+        }
+
         char currentChar = displayText.text[currentIndex];
         int charIndex = alphabet.IndexOf(currentChar);
 
         if (charIndex != -1)
         {
             charIndex = (charIndex + 1) % alphabet.Length;
-            char[] charArray = displayText.text.ToCharArray();
+            //char[] CharArray = displayText.text.ToCharArray();
             charArray[currentIndex] = alphabet[charIndex];
             displayText.text = new string(charArray);
         }
     }
 
-    // private void ConfirmSelection()
-    // {
-    //     currentIndex = (currentIndex + 1) % displayText.text.Length;
-    // }
+    private void ConfirmSelection()
+    {
+        currentIndex = (currentIndex + 1) % displayText.text.Length;
+    }
 }
