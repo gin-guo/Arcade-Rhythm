55c32aa932799e83ae0903a6bb2d2c4ff8475905
anniethemoody <annieli0428@gmail.com>
2024-03-08 19:57:23 -0500

    updated input change for username
    1. Right/left arrow go forward/go back
    2. up/down arrow to loop through option list, up: previous character, down: next character
    3. Flashing effect

--- Assets/Code/scoreboard/FlashText.cs
+++ Assets/Code/scoreboard/FlashText.cs
@@ -1,6 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.UI;
 using TMPro;
 
 public class FlashText : MonoBehaviour
@@ -8,6 +9,11 @@ public class FlashText : MonoBehaviour
     public TextMeshProUGUI displayText;
     private string alphabet = "abcdefghijklmnopqrstuvwxyz0123456789_";
     private int currentIndex = 0;
+    private float flashInterval = 0.5f;
+    private float flashTimer;
+    private bool isFlashing = true;
+    private char store = ' ';
+    private bool stop = false;
 
     private void Start()
     {
@@ -16,13 +22,33 @@ public class FlashText : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(!stop)
+            HandleFlashing();
+        //go to next position
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            char currentChar = displayText.text[currentIndex];
+            if (currentChar != ' ')
+                stop = true;
+        }
+        else if (!stop && Input.GetKeyDown(KeyCode.RightArrow))
         {
             ConfirmSelection();
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        //go to previous position
+        else if (!stop && Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            GoBackSelection();
+        }
+        //go to next character
+        else if (!stop && Input.GetKeyDown(KeyCode.DownArrow))
         {
             CycleNextCharacter();
+        }
+        //go to previous character
+        else if (!stop && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            CycleLastCharacter();
         }
     }
     
@@ -31,16 +57,10 @@ public class FlashText : MonoBehaviour
     {
         //replace _ with 'a'
         char[] charArray = displayText.text.ToCharArray();
-        //charArray[currentIndex];
-        if (charArray[currentIndex] == '_')
-        {
-            charArray[currentIndex] = alphabet[0];
-            displayText.text = new string(charArray);
-        }
-
         char currentChar = displayText.text[currentIndex];
+        if (currentChar == ' ')
+            currentChar = store;
         int charIndex = alphabet.IndexOf(currentChar);
-
         if (charIndex != -1)
         {
             charIndex = (charIndex + 1) % alphabet.Length;
@@ -48,10 +68,98 @@ public class FlashText : MonoBehaviour
             charArray[currentIndex] = alphabet[charIndex];
             displayText.text = new string(charArray);
         }
+        
+    }
+
+    private void CycleLastCharacter()
+    {
+        char[] charArray = displayText.text.ToCharArray();
+        char currentChar = displayText.text[currentIndex];
+        if (currentChar == ' ')
+            currentChar = store;
+        int charIndex = alphabet.IndexOf(currentChar);
+
+        if (charIndex != -1)
+        {
+            if (charIndex == 0)
+                charIndex = alphabet.Length - 1;
+            else
+                charIndex = (charIndex - 1) % alphabet.Length;
+            //char[] CharArray = displayText.text.ToCharArray();
+            charArray[currentIndex] = alphabet[charIndex];
+            displayText.text = new string(charArray);
+        }
+    }
+
+    private void GoBackSelection()
+    {
+        char[] charArray = displayText.text.ToCharArray();
+        char currentCharacter = charArray[currentIndex];
+        if (currentCharacter == ' ')
+            return;
+        // Stop flashing and move to the next character
+        isFlashing = false;
+        if (currentIndex == 0)
+            currentIndex = displayText.text.Length - 1;
+        else
+            currentIndex = (currentIndex - 1) % displayText.text.Length;
+        isFlashing = true;
+        flashTimer = 0f;
     }
 
     private void ConfirmSelection()
     {
+        char[] charArray = displayText.text.ToCharArray();
+        char currentCharacter = charArray[currentIndex];
+        if (currentCharacter == ' ')
+            return;
+        // Stop flashing and move to the next character
+        isFlashing = false;
         currentIndex = (currentIndex + 1) % displayText.text.Length;
+        isFlashing = true;
+        flashTimer = 0f;
+    }
+    
+    private void HandleFlashing()
+    {
+        // Check if the current character is selected or flashing during cycling
+        if (isFlashing)
+        {
+            flashTimer += Time.deltaTime;
+
+            // Toggle character visibility based on the flash interval
+            if (flashTimer >= flashInterval)
+            {
+                ToggleCharacterVisibility();
+                flashTimer = 0f;
+            }
+        }
+    }
+
+    private void ToggleCharacterVisibility()
+    {
+        char[] charArray = displayText.text.ToCharArray();
+        Debug.Log("current char array : " + charArray);
+        
+        // Toggle the visibility of the current character
+        char currentCharacter = charArray[currentIndex];
+        if(currentCharacter != ' ')
+            store = currentCharacter;
+        for (int i = 0; i < alphabet.Length; i++)
+        {
+            if (charArray[currentIndex] == alphabet[i])
+            {
+                //currentCharacter = alphabet[i];
+                charArray[currentIndex] = (charArray[currentIndex] == currentCharacter) ? ' ' : currentCharacter;
+                displayText.text = new string(charArray);
+                return;
+            }
+        }
+        charArray[currentIndex] = (charArray[currentIndex] == ' ') ? store : ' ';
+        // Debug.Log("Current Character: " + currentCharacter);
+        // charArray[currentIndex] = (charArray[currentIndex] == currentCharacter) ? ' ' : currentCharacter;
+
+        // Update the display text
+        displayText.text = new string(charArray);
     }
 }
