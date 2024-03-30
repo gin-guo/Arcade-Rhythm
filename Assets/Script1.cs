19f95fc1c4b7847086335d60c473b9c1a3ecfb9b
petrichor-2 <99967717+petrichor-2@users.noreply.github.com>
2024-03-30 12:08:26 -0400

    Read sliders and spinners into beatmap

--- Assets/Script1.cs
+++ Assets/Script1.cs
@@ -1,12 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Script1 : MonoBehaviour
 {
     public InputActionAsset actions;
-    private InputAction moveAction;
+    private InputAction beatAction;
     public TextAsset bad_apple;
     private string beatmapString;
     private int maxLength;
@@ -17,13 +16,22 @@ public class Script1 : MonoBehaviour
     public AudioSource musicSource;
     public float songTime;
 
+    [Flags] private enum ObjectTypes {
+        None = 0,
+        Circle = 1,         // bit 0
+        Slider = 1 << 1,    // bit 1
+        Combo = 1 << 2,     // bit 2
+        Spinner = 1 << 3,   // bit 3
+        Colour1 = 1 << 4,   // bit 4
+        Colour2 = 1 << 5,   // bit 5
+        Colour3 = 1 << 6,   // bit 6
+        Hold = 1 << 7,      // bit 7
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        /*
-        Color customColor = new Color(1, 1, 0);
-        GetComponent<SpriteRenderer>().material.SetColor("_Color", customColor);
-        */
+        actions.FindActionMap("gameplay").FindAction("press").performed += OnPress;
 
         beatmapString = bad_apple.text;
         string[] hitSection = beatmapString.Split("[HitObjects]");
@@ -31,45 +39,61 @@ public class Script1 : MonoBehaviour
 
         int row = 0;
         maxLength = hitObjectLines.Length - 1;
-        beats = new int[maxLength, 6];
+        beats = new int[maxLength, 5];
+        // beats -> [x, y, time, type, endTime]
+        // type: 0 (hit circle) or 1 (slider) or 3 (spinner)
         
+        // read beats into beat array
         for (int i = 1; i < hitObjectLines.Length; ++i)
         {    
             if (hitObjectLines[i] != "")
             {
                 string[] singleObject = hitObjectLines[i].Split(",");
-                if (singleObject[3] == "1" || singleObject[3] == "5")
+                int ObjectType = int.Parse(singleObject[3]);
+                if (((ObjectTypes) ObjectType & ObjectTypes.Circle) != 0)
                 {
                     // Debug.Log($"string<{string.Join(",", singleObject)}>string");
-                    for (int j = 0; j < singleObject.Length; ++j)
+                    for (int j = 0; j < 3; ++j)
                     {
                         beats[row, j] = int.Parse(singleObject[j]);
-                        // Debug.Log($"Row: {row}; Col: {j}; Val: {singleObject[j]}");
+                        Debug.Log($"Row: {row}; Col: {j}; Val: {singleObject[j]}");
                     }
+                    beats[row, 3] = 0;
+                    beats[row, 4] = 0;
+                    row++;
+                } else if (((ObjectTypes) ObjectType & ObjectTypes.Slider) != 0) {
+                    for (int j = 0; j < 3; ++j)
+                    {
+                        beats[row, j] = int.Parse(singleObject[j]);
+                        Debug.Log($"Row: {row}; Col: {j}; Val: {singleObject[j]}");
+                    }
+                    beats[row, 3] = 1;
+                    beats[row, 4] = -1;
+                    row++;
+                } else if (((ObjectTypes) ObjectType & ObjectTypes.Spinner) != 0) {
+                    for (int j = 0; j < 3; ++j)
+                    {
+                        beats[row, j] = int.Parse(singleObject[j]);
+                        Debug.Log($"Row: {row}; Col: {j}; Val: {singleObject[j]}");
+                    }
+                    beats[row, 3] = 3;
+                    beats[row, 4] = int.Parse(singleObject[5]);
                     row++;
                 }
             }
         }
 
-/*
-        foreach (var i in beats)
-        {
-            Debug.Log(i);
-        }
-
-        foreach (var hitObject in hitObjectLines)
-        {
-            string[] singleObject =  hitObjectLines.Split(,);
-            if (singleObject.Length < 6)
-            {
-                foreach (var value in singleObject)
-                {
-                    singleObject;
+        // write endTime for sliders (currently have a sentinel value of -1)
+        int buffer = 100; // buffer in ms between end of slider and start of next beat
+        for (int i = 0; i < maxLength; ++i) {
+            if (beats[i, 4] == -1) {
+                if (beats[i + 1, 2] - beats[i, 2] > buffer) {
+                    beats[i, 4] = beats[i + 1, 2] + buffer;
+                } else {
+                    beats[i, 4] = beats[i + 1, 2];
                 }
             }
-            // Debug.Log(hitObject);
         }
-        */
 
         musicSource = GetComponent<AudioSource>();
         startTime = (float) AudioSettings.dspTime;
@@ -83,12 +107,27 @@ public class Script1 : MonoBehaviour
         // Debug.Log(songTime);
         if (beats[t_index, 2] != 0) {
             if (songTime >= beats[t_index, 2]) {
-                Debug.Log("beat");
-                // Color customColor = new Color(0, 1, 0.5f);
-                GetComponent<SpriteRenderer>().material.SetColor("_Color", Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f));
+                if (beats[t_index, 3] == 0) {
+                    Debug.Log("circle beat");
+                    // Color customColor = new Color(0, 1, 0.5f);
+                    GetComponent<SpriteRenderer>().material.SetColor("_Color", UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f));
+                    transform.position = new Vector3(beats[t_index, 0] / 100, beats[t_index, 1] / 100);
+                }
                 t_index++;
             }
         }
-        
+    }
+    private void OnPress(InputAction.CallbackContext context)
+    {
+        Debug.Log("Button pressed!");
+    }
+
+    void OnEnable()
+    {
+        actions.FindActionMap("gameplay").Enable();
+    }
+    void OnDisable()
+    {
+        actions.FindActionMap("gameplay").Disable();
     }
 }
