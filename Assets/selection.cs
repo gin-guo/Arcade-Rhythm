dbc6abddaa67fa21509aafa70a64580e51f1a90c
victoriaaajiang <jurly.jiang@gmail.com>
2024-03-30 11:10:37 -0400

    highlighting outline added

---
+++ Assets/selection.cs
@@ -0,0 +1,17 @@
+/*using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(OutlineGenerator))]
+
+public class OutlineEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        OutlineGenerator generator = (OutlineGenerator)target;
+        if (GUILayout.Button("Generate Single Outline"))
+        {
+            generator.GenerateOutline();
+        }
+    }
+}*/
